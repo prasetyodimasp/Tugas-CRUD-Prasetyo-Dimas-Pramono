88ffec147832d019246bebed4e423e707b28e0cb
prasetyodimasp <prasetyodimasp2711@gmail.com>
2024-04-27 16:21:18 +0700

    HOC

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,8 +1,14 @@
 import  App  from "./App";
 import { createBrowserRouter } from "react-router-dom";
 import UserDetailPage from "./pages/user-detail-page";
+import withEditUser from "./components/hocs/withEditUser";
+import withAddUser from "./components/hocs/withAddUser";
+
+const EditUserPage =withEditUser(UserDetailPage);
+const AddUserPage = withAddUser(UserDetailPage);
 
 export const router = createBrowserRouter([
     {path: "/", element: <App />}, 
-    {path: "/:id", element:<UserDetailPage />},
+    {path: "/:id", element:<EditUserPage />},
+    {path: "/add", element:<AddUserPage />},
 ]);
