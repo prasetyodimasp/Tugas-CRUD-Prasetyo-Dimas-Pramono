74548a367c584d417c8406f50dea98e6c945c17f
prasetyodimasp <prasetyodimasp2711@gmail.com>
2024-04-27 15:23:16 +0700

    add routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routers.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/user-detail-page.tsx
@@ -0,0 +1,7 @@
+import React, {FC} from 'react'
+
+const UserDetailPage: FC = () => {
+  return <div>UserDetailPage</div>;
+};
+
+export default UserDetailPage;
